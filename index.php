78d9b350da6c775a186beabad789898b871b5ffb
Manish P <mkpandit@users.noreply.github.com>
2018-03-01 17:00:18 -0500

    Adding an index.php file for landing

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="assets/styles/style.css" />
+	<title>RESTful API implementation in PHP</title>
+</head>
+
+<body>
+	<header>
+		<h1>RESTful API</h1>
+	</header>
+</body>
+</html>
